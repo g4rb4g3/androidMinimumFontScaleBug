7583fd9ada7841ad3c8243e91a3696a0ae837f63
g4rb4g3 <g4rb4g3@gmail.com>
2023-06-22 09:36:53 +0200

    min reproducible

--- App.js
+++ App.js
@@ -1,12 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { useFonts, Inter_400Regular } from '@expo-google-fonts/inter';
 
 export default function App() {
+  const [ number, setNumber ] = useState(0);
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style='auto' />
+      <View style={{backgroundColor: 'black', height: 30}}>
+      {number > 0 ? (
+        <Text
+          adjustsFontSizeToFit={true}
+          allowFontScaling={true}
+          minimumFontScale={0.8571428571428571}
+          numberOfLines={2}
+          style={styles.text}
+        >
+          {number} Open up App.js to start working on your app!
+        </Text>
+        ) : null}
+        <Button onPress={() => setNumber((s) => s + 1)} title='+' />
+      </View>
     </View>
   );
 }
@@ -14,8 +38,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'gray',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    fontFamily: 'Inter_400Regular',
+    fontSize: 21,
+    fontWeight: '400',
+    textAlign: 'center'
   },
 });
